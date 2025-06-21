214afc16acea9967d9dfddc2f75f8d64dc5eed4d
PLManuel <manuel715pl@gmail.com>
2025-06-21 16:59:33 -0500

    agrega componente logo principal

---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,6 @@
+<span
+  {{ $attributes->merge(['class' => 'bg-gradient-to-r from-blue-600 to-indigo-600 block text-white h-auto w-8 rounded-lg p-1.5']) }}>
+  <svg fill="none" stroke="currentColor" viewBox="0 0 24 24">
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6" />
+  </svg>
+</span>
