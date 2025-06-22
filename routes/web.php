0bb438f5bf0d4fb834b781ebb5f16ddbbe1dea54
PLManuel <manuel715pl@gmail.com>
2025-06-21 19:17:14 -0500

    agrega nuevas rutas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\ArchivoController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CursoController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -16,8 +18,31 @@ Route::prefix('cursos')->name('cursos.')->group(function () {
     Route::get('/{codigo}/examen', [CursoController::class, 'examen'])->name('examen');
 });
 
-
 Route::resource('cursos.archivos', ArchivoController::class)->shallow();
+
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+// Route::middleware(['auth', 'admin'])->group(function () {
+//     Route::get('/dashboard', fn() => view('pages.dashboard'))->name('dashboard');
+// });
+
+Route::middleware(['auth', 'admin'])->prefix('dashboard')->group(function () {
+    Route::get('/', fn() => view('pages.dashboard.index'))->name('dashboard');
+    Route::get('/usuarios', [UserController::class, 'index'])->name('dashboard.usuarios');
+    Route::post('/usuarios', [UserController::class, 'store'])->name('dashboard.usuarios.store');
+    Route::delete('/usuarios/{user}', [UserController::class, 'destroy'])->name('dashboard.usuarios.destroy');
+    Route::get('/cursos', [CursoController::class, 'adminIndex'])->name('dashboard.cursos');
+    Route::post('/cursos', [CursoController::class, 'store'])->name('dashboard.cursos.store');
+    Route::delete('/cursos/{curso}', [CursoController::class, 'destroy'])->name('dashboard.cursos.destroy');
+    Route::post('/cursos/{curso}/archivos', [ArchivoController::class, 'store'])->name('cursos.archivos.store');
+    Route::delete('/archivos/{archivo}', [ArchivoController::class, 'destroy'])->name('archivos.destroy');
+});
 
 Route::get('/simuladores', function () {
     return view('pages.simuladores');
