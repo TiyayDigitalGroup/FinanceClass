caccd995575999916ee29547bd5e4919819b52a8
PLManuel <manuel715pl@gmail.com>
2025-06-27 21:28:29 -0500

    agrega seeder de cursos base

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RoleSeeder::class,
             AdminUserSeeder::class,
-            CourseSeeder::class,
+            // CourseSeeder::class,
+            MultiCoursesSeeder::class,
         ]);
     }
 }
