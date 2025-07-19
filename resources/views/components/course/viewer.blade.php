a5ab01944a38fd3aa99567789fedaad58065dea0
PLManuel <manuel715pl@gmail.com>
2025-07-18 20:07:23 -0500

    arregla el visor de imagenes, soporta imagenes de diferentes relaciones de aspectos

--- resources/views/components/course/viewer.blade.php
+++ resources/views/components/course/viewer.blade.php
@@ -5,23 +5,20 @@
 
 <div class="bg-white mt-4 border border-gray-200 rounded-2xl p-4 shadow-sm">
   @if (in_array($ext, ['jpg', 'jpeg', 'png', 'gif', 'webp']))
-    <img src="{{ $url }}" alt="{{ $archivo->title }}" class="rounded-lg max-w-full mx-auto h-[30rem]" />
-  
+    <img src="{{ $url }}" alt="{{ $archivo->title }}"
+      class="rounded-lg max-w-full mx-auto h-auto max-h-[30rem]" />
   @elseif ($ext === 'pdf')
     <iframe src="{{ $url }}" class="w-full h-[30rem] rounded-lg" frameborder="0"></iframe>
-
   @elseif (in_array($ext, ['mp4', 'webm', 'mov']))
     <video controls class="rounded-lg w-full max-h-[30rem]">
       <source src="{{ $url }}" type="video/{{ $ext }}">
       Tu navegador no soporta el video.
     </video>
-
   @elseif (in_array($ext, ['mp3', 'wav', 'ogg']))
     <audio controls class="w-full mt-2">
       <source src="{{ $url }}" type="audio/{{ $ext }}">
       Tu navegador no soporta el audio.
     </audio>
-
   @else
     <div class="text-gray-600 text-center">
       <p class="mb-2">Este tipo de archivo no puede visualizarse directamente.</p>
